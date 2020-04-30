4b1c20a10047092e30d5362040a9fbd89366118a
Ali <saida130.210@gmail.com>
2020-04-30 17:13:51 +0100

    proof of work

--- models/Block.js
+++ models/Block.js
@@ -3,30 +3,31 @@ const { computeHash } = require('../utils/computeHash')
 const { createChain } = require('../utils/chainHelpers')
 const Blockchain = require('./Blockchain')
 
-
-const DIFFICULTY = 2
+let { DIFFICULTY, MINE_RATE } = process.env
 
 const blockSchema = new Schema({
     timestamp: { type: String, default: Date.now },
     previousHash: { type: String },
     // transactions: [{}],
     data: { type: String },
-    chain: { type: Schema.Types.ObjectId, ref: 'Blockchain', default: '5ea47b69acd66cc296101ec1' },
+    chain: { type: Schema.Types.ObjectId, ref: 'Blockchain' },
     hash: { type: String, default: '' },
-    nonce: { type: Number, default: 0 }
+    nonce: { type: Number, default: 0 },
+    difficulty: { type: Number, default: DIFFICULTY }
 })
 
 blockSchema.method({
     mine: async function() {
-        let { timestamp, nonce, hash } = this
+        let { timestamp, hash, nonce } = this
         const blockchain = await Blockchain.findOrCreateOne()
         const latestBlock = blockchain.latestBlock()
-        const prevHash = await Block.findByIdAndReturnHash(latestBlock)
-        this.previousHash = prevHash
+        const previousHash = await Block.findByIdAndReturnHash(latestBlock)
+        console.log(DIFFICULTY++)
         while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY)) {
             nonce++
-            hash = computeHash(timestamp, prevHash, 'lol', nonce)
+            hash = computeHash(timestamp, previousHash, 'lol', nonce)
         }
+        this.previousHash = previousHash
         this.hash = hash
         this.nonce = nonce
         blockchain.chain.push(this)
